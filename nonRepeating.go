d91e9a61ee257b1903d70582430e41020776568f
Mahesh Jaisinghani <r.mahesh.jaisinghani@accenture.com>
2021-08-12 12:28:49 +0530

    updated repeating code

--- nonRepeating.go
+++ nonRepeating.go
@@ -1,24 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main(){
-	str:="stress"
+	str:="stresster"
 	_=str
 	chars:=[]rune(str)
 	var s []string
-	var b bool=false
+	var s1 []string
+	var str3 string
 	for i:=0;i<len(chars);i++ {
-		for j:=i+1;j>len(chars);j++{
-			if (chars[i]==chars[j]) {
-				b=true
-				continue
+		for j:=i+1;j<len(chars);j++{
+			if(chars[i]==chars[j]){
+				if !(strings.Contains(str3,string(chars[i]))){
+					s = append(s, string(chars[i]))
+					str3 = strings.Join(s, "")
+				}
+			}else{
+				s1=append(s1,string(chars[i]))
 			}
 		}
-		if(b==false){
-			s=append(s,string(chars[i]))
-		}
-
 	}
-	fmt.Println(s)
+	var s4 []string
+	for i:=0;i<len(s1);i++{
+		if !strings.Contains(str3,s1[i]){
+				s4=append(s4,s1[i])
+		}
+	}
+fmt.Println("Repeatative chars:",str3)
+	//fmt.Println(s1)
+	fmt.Println("Non repeatative chars",s4)
+
 }
